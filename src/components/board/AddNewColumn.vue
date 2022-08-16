314cbd8694e20208d95fc0548e4c1bbbbb27ad5d
Rodrigo Vergara <nosoyelpeluca@gmail.com>
2022-08-16 06:54:27 -0400

    Update AddNewColumn.vue

--- src/components/board/AddNewColumn.vue
+++ src/components/board/AddNewColumn.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex items-center min-h-full pt-11">
     <div @click="addNewColumn"
-      class="h-full flex items-center min-w-[280px] cursor-pointer bg-gradient-to-b from-dark-grey to-dark-grey/25 rounded-md">
-      <p class="text-2xl text-center w-full text-medium-grey font-bold select-none">+ New Column</p>
+      class="h-full flex items-center min-w-[280px] cursor-pointer bg-gradient-to-b from-[#E9EFFA] to-[#E9EFFA]/50 dark:from-dark-grey dark:to-dark-grey/25 rounded-md text-medium-grey hover:text-main-purple ">
+      <p class="text-2xl text-center w-full font-bold select-none">+ New Column</p>
     </div>
   </div>
 </template>
