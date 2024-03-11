fd734205f4073fa346a9d8d370ea124620a0742e
Rodrigo Vergara <67773102+UnTalPeluca@users.noreply.github.com>
2024-03-10 21:38:16 -0300

    Dropdown hover effects and shadow

--- src/components/manager/Dropdown.vue
+++ src/components/manager/Dropdown.vue
@@ -1,10 +1,10 @@
 <template>
   <div tabindex="1" class="group cursor-pointer relative">
     <IconVerticalEllipsis />
-    <div class="absolute right-28 top-8 hidden group-focus:block">
-      <div class="bg-white dark:bg-very-dark-grey rounded-lg text-sm w-36 flex flex-col fixed z-10">
-        <p @click="emit('onClickEdit', target)" class="text-medium-grey text-xs font-bold w-full p-4 ">Edit {{ target }}</p>
-        <p @click="emit('onClickDelete', target)" class="text-red text-xs font-bold w-full p-4 ">Delete {{ target }}</p>
+    <div class="absolute right-36 top-8 hidden group-focus:block">
+      <div class="bg-white dark:bg-very-dark-grey rounded-lg text-sm w-36 flex flex-col fixed z-10 shadow-lg">
+        <p @click="emit('onClickEdit', target)" class="text-medium-grey hover:text-main-purple dark:hover:text-light-grey  text-xs font-bold w-full p-4 ">Edit {{ target }}</p>
+        <p @click="emit('onClickDelete', target)" class="text-red hover:text-red-light text-xs font-bold w-full p-4 ">Delete {{ target }}</p>
       </div>
     </div>
   </div>
